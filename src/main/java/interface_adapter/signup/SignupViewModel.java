17e9a53bf1fc3ddbb619bf913c044410cfef7f87
Andy <azhang19642@gmail.com>
2023-11-18 15:50:04 -0500

    modify labels in SignupViewModel; rename, add new ones, remove redundant

--- src/main/java/interface_adapter/signup/SignupViewModel.java
+++ src/main/java/interface_adapter/signup/SignupViewModel.java
@@ -6,15 +6,14 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SignupViewModel extends ViewModel {
-
-    public static final String CLEAR_BUTTON_LABEL = "Clear";
-    public static final String TITLE_LABEL = "Sign Up View";
-    public static final String USERNAME_LABEL = "Choose username";
-    public static final String PASSWORD_LABEL = "Choose password";
-    public static final String REPEAT_PASSWORD_LABEL = "Enter password again";
+    public static final String USERNAME_LABEL = "Username";
+    public static final String PASSWORD_LABEL = "Password";
+    public static final String REPEAT_PASSWORD_LABEL = "Confirm password";
 
     public static final String SIGNUP_BUTTON_LABEL = "Sign up";
-    public static final String CANCEL_BUTTON_LABEL = "Cancel";
+
+    public static final String GO_TO_LOGIN_BUTTON_LABEL = "Go to login";
+    public static final String HAVE_ACCOUNT_LABEL = "Already a user?";
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     private SignupState state = new SignupState();
 
