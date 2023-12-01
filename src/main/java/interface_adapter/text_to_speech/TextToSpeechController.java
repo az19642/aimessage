c8fec621c813b3c0c72171a2a37ffb6e611abfbc
jchriso <jchrisoh@gmail.com>
2023-12-01 11:55:39 -0500

    add javadocs for everything public

--- src/main/java/interface_adapter/text_to_speech/TextToSpeechController.java
+++ src/main/java/interface_adapter/text_to_speech/TextToSpeechController.java
@@ -2,13 +2,28 @@ package interface_adapter.text_to_speech;
 
 import use_case.text_to_speech.TextToSpeechInputBoundary;
 
+/**
+ * Controller class for text-to-speech functionality.
+ * This class serves as an intermediary between the application interface
+ * and the text-to-speech use case.
+ */
 public class TextToSpeechController {
     private final TextToSpeechInputBoundary textoSpeechUseCaseInteractor;
 
+    /**
+     * Constructs a new TextToSpeechController.
+     *
+     * @param textoSpeechUseCaseInteractor The text-to-speech use case interactor to be used.
+     */
     public TextToSpeechController(TextToSpeechInputBoundary textoSpeechUseCaseInteractor) {
         this.textoSpeechUseCaseInteractor = textoSpeechUseCaseInteractor;
     }
 
+    /**
+     * Executes the text-to-speech functionality with the given message.
+     *
+     * @param message The text message to be converted to speech.
+     */
     public void execute(String message) {
         textoSpeechUseCaseInteractor.execute(message);
     }
--- src/main/java/interface_adapter/text_to_speech/TextToSpeechViewModel.java
+++ src/main/java/interface_adapter/text_to_speech/TextToSpeechViewModel.java
@@ -5,19 +5,34 @@ import interface_adapter.ViewModel;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * ViewModel class for text-to-speech functionality.
+ * Extends the base ViewModel class and provides functionality for managing the state of text-to-speech operations.
+ */
 public class TextToSpeechViewModel extends ViewModel {
     private TextToSpeechState state = new TextToSpeechState();
 
+    /**
+     * Constructs a new TextToSpeechViewModel with a default view name.
+     */
     public TextToSpeechViewModel() {
         super("Create speech from text.");
     }
 
+    /**
+     * Sets the state of the text-to-speech ViewModel.
+     *
+     * @param state The new state to set.
+     */
     public void setState(TextToSpeechState state) {
         this.state = state;
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    /**
+     * Fires a property change event to notify listeners about changes in the ViewModel state.
+     */
     @Override
     public void firePropertyChanged() {support.firePropertyChange("state", null, this.state);}
 
@@ -32,9 +47,9 @@ public class TextToSpeechViewModel extends ViewModel {
     }
 
     /**
-     * Gets the current state of the password generator.
+     * Gets the current state of the text-to-speech ViewModel.
      *
-     * @return The current state of the password generator.
+     * @return The current state of the text-to-speech ViewModel.
      */
     public TextToSpeechState getState() {
         return state;
