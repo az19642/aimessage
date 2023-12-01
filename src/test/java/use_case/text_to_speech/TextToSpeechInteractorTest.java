c696600495ed300cd7dafdbc42357337ecb32fe8
jchriso <jchrisoh@gmail.com>
2023-12-01 11:56:48 -0500

    write tests for text to speech interactor

--- src/test/java/use_case/text_to_speech/TextToSpeechInteractorTest.java
+++ src/test/java/use_case/text_to_speech/TextToSpeechInteractorTest.java
@@ -1,4 +1,32 @@
+package use_case.text_to_speech;
+
+import data_access.GPTDataAccessObject;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TextToSpeechInteractorTest {
-  
+
+    @Test
+    void successTest() {
+        TextToSpeechInputData inputData = new TextToSpeechInputData();
+        TextToSpeechDataAccessInterface textToSpeechDataAccessInterface =
+                new GPTDataAccessObject(System.getenv("OPENAI_API_KEY"));
+
+        TextToSpeechOutputBoundary successPresenter = new TextToSpeechOutputBoundary() {
+            @Override
+            public void prepareSuccessView(TextToSpeechOutputData response) {
+                assertTrue(response.getSpeechResult(), "Output should be true");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure is unexpected");
+            }
+        };
+
+        TextToSpeechInputBoundary interactor =
+                new TextToSpeechInteractor(textToSpeechDataAccessInterface, successPresenter);
+        interactor.execute("This is a test.");
+    }
 }
